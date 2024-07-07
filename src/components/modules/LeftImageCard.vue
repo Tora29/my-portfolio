a65c49c172059edf9e1d3d8129e77883bfd50619
tora29 <nikumann.power@gmail.com>
2024-07-07 22:00:07 +0900

    プロフィールページに追記

---
+++ src/components/modules/LeftImageCard.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="card">
+    <img :src="imageSrc" :alt="imageAlt" class="card-image">
+    <div class="card-content">
+      <h3>{{ title }}</h3>
+      <p v-html="description"></p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LeftImageCard',
+  props: {
+    imageSrc: {
+      type: String,
+      required: true
+    },
+    imageAlt: {
+      type: String,
+      default: 'Card image'
+    },
+    title: {
+      type: String,
+      required: true
+    },
+    description: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+.card {
+  /* グリッドデザイン設定 */
+  display: grid;
+  grid-template-columns: 4fr 8fr;
+  
+  /* 枠線設定 */
+  border: 1px solid #ccc;
+  border-radius: 8px;
+  overflow: hidden;
+  max-width: 100%;
+  box-sizing: border-box;
+}
+
+.card-image {
+  width: 90%;
+  height: auto;
+}
+
+.card-content {
+  padding: 16px;
+}
+
+.card-content h3 {
+  margin: 0;
+  font-size: 1.5em;
+}
+
+.card-content p {
+  margin: 8px 0 0;
+}
+</style>
--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -1,18 +1,44 @@
 <template>
   <MainLayout>
     <template v-slot>
-      <div>プロフィールのコンテンツ</div>
+      <h3>Profile</h3>
+      <LeftImageCard
+        :imageSrc="tigerIcon"
+        imageAlt="虎のアイコン"
+        title="tora29"
+        :description="formattedDescription"
+      />
     </template>
   </MainLayout>
 </template>
 
 <script>
-import MainLayout from '../components/layouts/MainLayout.vue';
+import MainLayout from "../components/layouts/MainLayout.vue";
+import LeftImageCard from "../components/modules/LeftImageCard.vue";
+import { PROFILE_DESCRIPTION } from "../constants/texts";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
     MainLayout,
+    LeftImageCard,
   },
+  data() {
+    return {
+      tigerIcon: require("@/assets/img/tiger_icon.png"),
+      description: PROFILE_DESCRIPTION,
+      formattedDescription: ""
+    };
+  },
+  mounted() {
+    this.formattedDescription = this.description.replace(/\n/g, '<br>');
+  }
 };
 </script>
+
+<style scoped>
+h3 {
+  text-align: center;
+  padding-bottom: 12px;
+}
+</style>
