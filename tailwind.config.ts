03d8449f2e7ec6da6c8a62e5b62406e90903f4a5
tora29 <nikumann.power@gmail.com>
2024-11-27 20:40:05 +0900

    initial commit

---
+++ tailwind.config.ts
@@ -0,0 +1,14 @@
+import daisyui from 'daisyui';
+import type { Config } from 'tailwindcss';
+
+export default {
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+
+	theme: {
+		extend: {}
+	},
+
+	plugins: [
+		daisyui,
+	]
+} satisfies Config;
