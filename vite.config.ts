0c003bde671fbc787f7dc03805f72e3442361352
tora29 <nikumann.power@gmail.com>
2024-12-13 23:57:01 +0900

    テストの設定を追加

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,15 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
 
 /// <reference types="vitest" />
-export default defineConfig({
+// https://github.com/sveltejs/svelte/issues/11394
+export default defineConfig(({ mode }) => ({
 	plugins: [sveltekit()],
 	test: {
 		globals: true,
 		environment: 'jsdom',
 		include: ['src/**/*.test.ts']
+	},
+	resolve: {
+		conditions: mode === 'test' ? ['browser'] : []
 	}
-});
+}));
